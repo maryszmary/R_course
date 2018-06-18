e05f35bd66509393ec6cf87d96f297b93f1b167b
maryszmary <masha.shejanova@gmail.com>
2018-06-19 02:31:08 +0300

    bugfix

--- project/collect_data.r
+++ project/collect_data.r
@@ -4,13 +4,16 @@ library(udpipe)
 udmodel <- udpipe_download_model(language = "russian")
 udmodel <- udpipe_load_model(file = udmodel$file_model)
 txt = c("Это мой тестовый текст. Посмотрм, как ты справишься с ним", "Как тебе такое, Элон Маск?")
+names(txt) = c('a', 'b')
 x <- udpipe_annotate(udmodel, x = txt)
 x <- as.data.frame(x)
 str(x)
 
 setwd('~/Documents/R_course/project/')
-write.table(x, "trial.csv", sep = '\t')
+# write.table(x, "trial.csv", sep = '\t')
+write.csv(x, 'tr.csv')
 
+tr <- read.csv('tr.csv')
 
 ## getting the data
 
@@ -27,4 +30,6 @@ read_parse_write <- function(rpath, wpath, udmodel) {
   write.table(x, wpath, sep = '\t')
 }
 
-read_parse_write("txt/onegin.txt", "data/poetry/pushkin_ong.csv", udmodel)
+# read_parse_write("txt/onegin.txt", "data/poetry/pushkin_ong.csv", udmodel)
+
+pushkin_fiction
